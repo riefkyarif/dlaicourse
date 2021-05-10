b1ef8d2f49eeef6ae77b3a3e44275382fbf607ff
Riefky Arif I <80331973+riefkyarif@users.noreply.github.com>
2021-05-10 12:15:05 +0700

    Update index_exercise.js

--- TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
+++ TensorFlow Deployment/Course 1 - TensorFlow-JS/Week 4/Exercise/index_exercise.js
@@ -27,18 +27,33 @@ async function train() {
     layers: [
         
       // YOUR CODE HERE
-
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({
+        units: 100,
+        activation: "relu"
+      }),
+      tf.layers.dense({
+        units: 5,
+        activation: "softmax"
+      })
     ]
   });
     
    
   // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = // YOUR CODE HERE
+  const optimizer = tf.train.adam(0.0001); // YOUR CODE HERE
     
         
   // Compile the model using the categoricalCrossentropy loss, and
   // the optimizer you defined above.
-  model.compile(// YOUR CODE HERE);
+	
+  // YOUR CODE HERE);
+  model.compile({
+    loss: "categoricalCrossentropy",
+    optimizer: tf.train.adam(0.0001)
+  }); 
+  model.summary();
+  
  
   let loss = 0;
   model.fit(dataset.xs, dataset.ys, {
@@ -71,11 +86,16 @@ function handleButton(elem){
 			spockSamples++;
 			document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
 			break;
+		// YOUR CODE HERE
+    		case "4":
+      			lizardSamples++;
+      			document.getElementById("lizardsamples").innerText = "SpockLizard samples:" + lizardSamples;
+      			break;
             
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
             
-        // YOUR CODE HERE
+       
 		
             
 	}
@@ -108,13 +128,14 @@ async function predict() {
 		case 3:
 			predictionText = "I see Spock";
 			break;
+		// YOUR CODE HERE 
+      		case 4:
+        		predictionText = "I see Lizard";
+        		break;
             
         // Add a case for lizard samples.
         // HINT: Look at the previous cases.
-            
-        // YOUR CODE HERE 
-	
-            
+  
 	}
 	document.getElementById("prediction").innerText = predictionText;
 			
